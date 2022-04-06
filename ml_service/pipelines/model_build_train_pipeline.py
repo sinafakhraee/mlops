e60fdc36d9333fc273cf968f6c637a8820a12e99
Sina <sifakhra@microsoft.com>
2022-04-06 14:55:20 -0400

    updated

--- ml_service/pipelines/model_build_train_pipeline.py
+++ ml_service/pipelines/model_build_train_pipeline.py
@@ -3,7 +3,6 @@ from azureml.pipeline.steps import PythonScriptStep
 from azureml.pipeline.core import Pipeline, PipelineData
 from azureml.core import Workspace, Dataset, Datastore
 from azureml.core.runconfig import RunConfiguration
-from ml_service.pipelines.load_sample_data import create_sample_data_csv
 from ml_service.util.attach_compute import get_compute
 from ml_service.util.env_variables import Env
 from ml_service.util.manage_environment import get_environment
